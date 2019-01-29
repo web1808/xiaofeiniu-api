99281ff4452e5a2f8037887af42fbadc26de5ad6
LiWenhua <lwh@tedu.cn>
2019-01-29 15:36:57 +0800

    核心功能完成

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const categoryRouter = require('./routes/admin/category');
 const adminRouter = require('./routes/admin/admin');
+const dishRouter = require('./routes/admin/dish');
 
 //创建HTTP应用服务器
 var app = express(); 
@@ -22,3 +23,4 @@ app.use(bodyParser.json()); //把application/json格式的请求主体数据解�
 //挂载路由器
 app.use('/admin/category', categoryRouter);
 app.use('/admin', adminRouter);
+app.use('/admin/dish', dishRouter);
